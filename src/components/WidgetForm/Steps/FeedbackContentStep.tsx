d8d310e1df74f07652c2cfb45c8e2514e365cfcf
Talisson Oliveira <talissonlux@outlook.com>
2022-05-04 02:36:59 -0300

    feat: creates the screenshot component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft, Camera } from 'phosphor-react'
+import { ArrowLeft } from 'phosphor-react'
+import { useState } from 'react'
 import { FeedbackType, feedbackTypes } from '..'
 import { CloseButton } from '../../CloseButton'
+import { ScreenshotButton } from '../ScreenshotButton'
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType
@@ -11,6 +13,7 @@ export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   return (
@@ -43,12 +46,7 @@ export function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="bg-zinc-800 p-2 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6" />
-          </button>
+          <ScreenshotButton screenshot={screenshot} onScreenshotTook={setScreenshot} />
 
           <button
             type="submit"
